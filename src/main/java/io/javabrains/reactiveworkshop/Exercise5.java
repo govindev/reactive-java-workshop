495dd940820e5e866c8a4a57feae3783eba1901c
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-11-13 14:33:42 +0530

    Controlling backpressure with subscription classes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,6 +1,10 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
+import java.util.concurrent.Flow;
 
 public class Exercise5 {
 
@@ -9,21 +13,36 @@ public class Exercise5 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        ReactiveSources.intNumbersFlux().subscribe(
-                (number) -> System.out.println(number),
-                (error) -> System.out.println(error.getMessage()),
-                () -> System.out.println("Complete")
-        );
+//        ReactiveSources.intNumbersFlux().subscribe(
+//                (number) -> System.out.println(number),
+//                (error) -> System.out.println(error.getMessage()),
+//                () -> System.out.println("Complete")
+//        );
+//
+//
+//        ReactiveSources.userMono().subscribe(
+//                (user) -> System.out.println(user),
+//                (error) -> System.out.println(error.getMessage()),
+//                () -> System.out.println("Complete")
+//        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        ReactiveSources.userMono().subscribe(
-                (user) -> System.out.println(user),
-                (error) -> System.out.println(error.getMessage()),
-                () -> System.out.println("Complete")
-        );
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+    public void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribe happened");
+        request(1); // Initially we request to receive 1
+    }
+
+    public void hookOnNext(T value) {
+        System.out.println(value.toString() + " received");
+        request(1); // When receiving one we request to receive another
+    }
 }
