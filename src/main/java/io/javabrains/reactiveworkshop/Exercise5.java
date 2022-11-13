797a5bc70d42c4be39d924691342eecda3c72bce
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-11-13 14:14:49 +0530

    subscribe method with complete and failure handlers

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -6,13 +6,21 @@ public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(
+                (number) -> System.out.println(number),
+                (error) -> System.out.println(error.getMessage()),
+                () -> System.out.println("Complete")
+        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.userMono().subscribe(
+                (user) -> System.out.println(user),
+                (error) -> System.out.println(error.getMessage()),
+                () -> System.out.println("Complete")
+        );
 
         System.out.println("Press a key to end");
         System.in.read();
