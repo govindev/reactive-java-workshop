01df62735c1d873a7ae02f28cfed03ffc6bc83d2
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-11-18 21:31:20 +0530

    Flux to Mono

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Exercise9 {
 
@@ -10,13 +11,24 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        AtomicInteger count = new AtomicInteger();
+        ReactiveSources.intNumbersFlux()
+                .subscribe(val -> count.getAndIncrement(),
+                        System.out::println,
+                        () -> System.out.println("Length is " + count));
+
+        // The right way to do is using the mono
+        ReactiveSources.intNumbersFlux().count().subscribe(System.out::println);
+
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().collectList().subscribe(System.out::println);
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+                        .buffer(2)
+                                .map(list -> list.get(0) + list.get(1))
+                                        .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
